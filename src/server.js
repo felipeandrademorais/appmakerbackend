6e951fe46f2b8c82b27cbd572e11639cf2864d8d
FELIPE ANDRADE MORAIS <felipe.m.tecnico@sistemafiesc.org.br>
2019-08-14 18:46:42 -0300

    Add cors config

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require("cors");
 
 const app = express();
 
@@ -10,6 +11,7 @@ mongoose.connect('mongodb://felipe:feli2705@ds061391.mlab.com:61391/goweek-felip
     }
 );
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
