21214824b905ee94b1c690ad57b5631c93acb44c
FELIPE ANDRADE MORAIS <felipe.m.tecnico@sistemafiesc.org.br>
2019-08-14 18:38:19 -0300

    Add Answer for get

--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,8 @@ const AnswersController = require('./controller/AnswersController');
 
 routes.get('/answers', AnswersController.index);
 routes.post('/answers', AnswersController.store);
+routes.get('/', (req, res) => {
+    res.send({ message: "Server is runing" });
+});
 
 module.exports = routes;
